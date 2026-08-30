647fd8f7e44ba04bd38867815ba5d95287e936d7
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-08-29 22:54:23 -0700

    Export complete OMEGA V6 sovereign runtime surface

--- canonforge-omega/omega_runtime/__init__.py
+++ canonforge-omega/omega_runtime/__init__.py
@@ -1,8 +1,17 @@
 from .state import Address20736, EvidenceClass, MotionState, StateEnvelope, StateMetrics
 from .mode188 import Dispatch, Admission, GateDecision, OperatorDecision, gate, operator_decision
 from .runtime import OmegaRuntime
+from .state_store import StateStore
+from .scales import Address144, Address1728
+from .relations import RelationalState
+from .memory import ContradictionMemory, ScarRecord, ptf_core
+from .dynamics import MotionParameters, step_motion
+from .translation import TranslationOperator, TranslationResult, apply_translation
+from .knowledge import KnowledgeIndex, KnowledgeRecord, KnowledgeHit
 
 __all__ = [
-    "Address20736", "EvidenceClass", "MotionState", "StateEnvelope", "StateMetrics",
-    "Dispatch", "Admission", "GateDecision", "OperatorDecision", "gate", "operator_decision", "OmegaRuntime"
+    "Address144", "Address1728", "Address20736", "EvidenceClass", "MotionState", "StateEnvelope", "StateMetrics",
+    "Dispatch", "Admission", "GateDecision", "OperatorDecision", "gate", "operator_decision", "OmegaRuntime", "StateStore",
+    "RelationalState", "ContradictionMemory", "ScarRecord", "ptf_core", "MotionParameters", "step_motion",
+    "TranslationOperator", "TranslationResult", "apply_translation", "KnowledgeIndex", "KnowledgeRecord", "KnowledgeHit"
 ]
