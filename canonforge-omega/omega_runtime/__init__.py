35ae6a76e4a3ac70d64e9fe07aa06d455e31d836
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-06 13:43:13 -0700

    R170 export advanced computation kernel

--- canonforge-omega/omega_runtime/__init__.py
+++ canonforge-omega/omega_runtime/__init__.py
@@ -8,10 +8,26 @@ from .memory import ContradictionMemory, ScarRecord, ptf_core
 from .dynamics import MotionParameters, step_motion
 from .translation import TranslationOperator, TranslationResult, apply_translation
 from .knowledge import KnowledgeIndex, KnowledgeRecord, KnowledgeHit
+from .advanced_computation import (
+    C_M_S,
+    COMPUTE_SCHEMA,
+    OpticalLayer,
+    computation_manifest,
+    conservative_diffusion_step,
+    conservative_transfer,
+    lorentz_boost_event,
+    normal_incidence_tmm,
+    run_truth_suite,
+    scalar_wave_fdtd_1d,
+    transform_velocity,
+)
 
 __all__ = [
     "Address144", "Address1728", "Address20736", "EvidenceClass", "MotionState", "StateEnvelope", "StateMetrics",
     "Dispatch", "Admission", "GateDecision", "OperatorDecision", "gate", "operator_decision", "OmegaRuntime", "StateStore",
     "RelationalState", "ContradictionMemory", "ScarRecord", "ptf_core", "MotionParameters", "step_motion",
-    "TranslationOperator", "TranslationResult", "apply_translation", "KnowledgeIndex", "KnowledgeRecord", "KnowledgeHit"
+    "TranslationOperator", "TranslationResult", "apply_translation", "KnowledgeIndex", "KnowledgeRecord", "KnowledgeHit",
+    "C_M_S", "COMPUTE_SCHEMA", "OpticalLayer", "computation_manifest", "conservative_diffusion_step",
+    "conservative_transfer", "lorentz_boost_event", "normal_incidence_tmm", "run_truth_suite", "scalar_wave_fdtd_1d",
+    "transform_velocity",
 ]
