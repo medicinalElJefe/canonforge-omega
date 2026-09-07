b799ddc4d6887c1d25083df247650fa6d1c06afe
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-06 22:15:59 -0700

    R193 preserve exact R192 wrapper compatibility

--- canonforge-omega/cloudflare/omega-v6-worker/src/runtimeEntryR169.ts
+++ canonforge-omega/cloudflare/omega-v6-worker/src/runtimeEntryR169.ts
@@ -130,10 +130,9 @@ async function runtimeFetch(request: Request, env: any, ctx: any): Promise<Respo
 }
 
 async function publicFetch(request: Request, env: any, ctx: any): Promise<Response> {
-  const pathname = new URL(request.url).pathname;
   const response = await runtimeFetch(request, env, ctx);
-  const r192 = await enhanceUniversalNavigationR192(response, pathname);
-  return enhanceUniversalWorkspaceR193(r192, pathname);
+  const r192 = await enhanceUniversalNavigationR192(response, new URL(request.url).pathname);
+  return enhanceUniversalWorkspaceR193(r192, new URL(request.url).pathname);
 }
 
 export default { fetch: publicFetch };
