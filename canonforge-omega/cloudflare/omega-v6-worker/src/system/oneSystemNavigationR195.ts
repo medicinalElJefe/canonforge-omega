993d25dee870b33143c4315b58187134fe2954ec
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-07 09:18:54 -0700

    R199 attach unified operator console to authoritative shell

--- canonforge-omega/cloudflare/omega-v6-worker/src/system/oneSystemNavigationR195.ts
+++ canonforge-omega/cloudflare/omega-v6-worker/src/system/oneSystemNavigationR195.ts
@@ -1,6 +1,7 @@
 import { EVIDENCE_PLANE_RELEASE_R194 } from "../evidencePlaneR194";
 import { reconstituteOneSystemR199, ONE_SYSTEM_CORRELATION_RELEASE_R199 } from "./oneSystemCorrelationR199";
 import { correlateOneSystemTruthStripR199 } from "./oneSystemTruthStripR199";
+import { enhanceOneSystemOperatorSurfaceR199 } from "./oneSystemOperatorSurfaceR199";
 
 export const ONE_SYSTEM_NAVIGATION_RELEASE_R195 = "r195-drive-corpus-one-system";
 
@@ -38,5 +39,6 @@ export async function enhanceOneSystemNavigationR195(response: Response, pathnam
   headers.set("x-omega-one-system-correlation", ONE_SYSTEM_CORRELATION_RELEASE_R199);
   const preserved = new Response(html, { status: response.status, statusText: response.statusText, headers });
   const reconstituted = await reconstituteOneSystemR199(preserved, pathname);
-  return correlateOneSystemTruthStripR199(reconstituted);
+  const correlated = await correlateOneSystemTruthStripR199(reconstituted);
+  return enhanceOneSystemOperatorSurfaceR199(correlated);
 }
