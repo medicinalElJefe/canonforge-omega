79965b2d526415fefdad304309ee29c3add3886e
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-07 10:28:06 -0700

    R201 preserve canonical OmegaRuntime export and add isolated ledger class

--- canonforge-omega/cloudflare/omega-v6-worker/src/runtimeEntryR169.ts
+++ canonforge-omega/cloudflare/omega-v6-worker/src/runtimeEntryR169.ts
@@ -52,7 +52,8 @@ import { handleEarthSarFusionR198 } from "./earthSarTruthFusionR198";
 import { enhanceEarthSarIntegratedRepairR198_1 } from "./earthSarIntegratedRepairR198_1";
 import { enhanceEarthSarVisualContextR198_2 } from "./earthSarVisualContextR198_2";
 
-export { OmegaRuntime } from "./system/omegaRuntimeR201";
+export { OmegaRuntime } from "./heartbeatTruth";
+export { OmegaRuntime as OmegaMissionLedgerR201 } from "./system/omegaRuntimeR201";
 export { OmegaSwarmCell } from "./swarm/swarmCellR169";
 export { OmegaSwarmCoordinatorR188 as OmegaSwarmCoordinator } from "./swarm/motionCoordinatorR188";
 export { OmegaSwarmBranch, OmegaSwarmOrgan, OmegaSwarmOrganismCoordinator } from "./swarm/swarmOrganismR169";
