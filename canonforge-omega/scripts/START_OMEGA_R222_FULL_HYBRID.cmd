a80c056313f8750762aff3de146c4e5767ceb64c
Jeffrey A Dewey <b6qrfb57ky@privaterelay.appleid.com>
2026-09-07 22:24:12 -0700

    R222 route release command through stable sovereign gateway

--- canonforge-omega/scripts/START_OMEGA_R222_FULL_HYBRID.cmd
+++ canonforge-omega/scripts/START_OMEGA_R222_FULL_HYBRID.cmd
@@ -2,5 +2,5 @@
 setlocal EnableExtensions
 chcp 65001 >nul
 set "OMEGA_ROOT_OVERRIDE=%~dp0.."
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0START_OMEGA_R222_FULL_HYBRID.ps1"
+call "%~dp0START_OMEGA_SOVEREIGN.cmd" %*
 exit /b %ERRORLEVEL%
